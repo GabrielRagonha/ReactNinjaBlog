77ca52731ad0c98c1ad2b31bb816fa234f04845e
Gabriel Rodrigues <gabrielRagonhaRodrigues@outlook.com>
2021-09-10 13:25:41 -0300

    Lesson 13: Functions as Props

--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,17 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'Gabriel', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Felipe', id: 2 },
-    { title: 'Web Dev top tips', body: 'lorem ipsum...', author: 'Gabriel', id: 3 }
+    { title: 'Web Dev top tips', body: 'lorem ipsum...', author: 'Jorge', id: 3 }
   ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'Gabriel')} title="Blogs do Gabriel" />
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
     </div>
   );
 }
